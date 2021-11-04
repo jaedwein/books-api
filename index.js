bdae190ce55e54d6af29d30884cc23d464f08d43
jaedwein <jaedwein@gmail.com>
2021-11-04 00:42:14 -0300

    Seguimos trabajando

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const helmet = require("helmet");
+const http = require("http");
+var compression = require("compression");
+const cors = require("cors");
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
+app.use(helmet());
+app.use(compression());
+app.use(cors());
 
 const books = [
   { title: "Java Programming", id: 1 },
@@ -11,7 +18,16 @@ const books = [
 ];
 
 app.get("/", (req, res) => {
+  console.log("Hola ....");
   res.send("Hola Mundo");
+});
+
+app.get("/api/books", (req, res) => {
+  res.send(books);
+});
+
+app.get("/api/books/:id", (req, res) => {
+  res.send(books.find((x) => x.id === parseInt(req.params.id)));
 });
 
 app.listen(8080);
