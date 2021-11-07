6355af7e8b2507c352cb4e1e4fd525db08bfd79d
jaedwein <jaedwein@gmail.com>
2021-11-07 12:05:44 -0300

    Se agrega documentación con swagger

--- mongodb.js
+++ mongodb.js
@@ -1,14 +1,79 @@
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
+const swaggerui = require("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc");
 
 const app = express();
 app.use(express.json());
 
 var database;
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node JS API project for MongoDB",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  apis: ["./mongodb.js"],
+};
+
+const swaggerSpecs = swaggerJSDoc(options);
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(swaggerSpecs));
+
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          Book:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                  title:
+ *                      type: string
+ *          DeleteResult:
+ *              type: object
+ *              properties:
+ *                  acknowledged:
+ *                      type: boolean
+ *                  deletedCount:
+ *                      type: integer
+ */
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      summary: this API is used to ckeck if the method is working or not.
+ *      description: this API is used to ckeck if the method is working or not.
+ *      responses:
+ *          200:
+ *              description: to test get method
+ */
+
 app.get("/", (_req, res) => {
   res.send("Bienvenido a MongoDB API");
 });
+
+/**
+ * @swagger
+ * /api/books/:
+ *  get:
+ *      summary: this API is used to get all books
+ *      description: this API is used to get all books
+ *
+ *      responses:
+ *          200:
+ *              description: returns all books
+ *              content:
+ */
 
 app.get("/api/books", (_req, res) => {
   database
@@ -23,6 +88,31 @@ app.get("/api/books", (_req, res) => {
     });
 });
 
+/**
+ * @swagger
+ * /api/books/{id}:
+ *  delete:
+ *      summary: this API is used to delete a book
+ *      description: this API is used to delete a book
+ *      parameters:
+ *          - in : path
+ *            name: id
+ *            required: true
+ *            description: id number of book to delete
+ *            schema:
+ *              type: integer
+ 
+ *      responses:
+ *          200:
+ *              description: returns a book
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#components/schemas/DeleteResult'
+ *          500:
+ *              description: error
+ */
+
 app.delete("/api/books/:id", (req, res) => {
   const id = parseInt(req.params.id);
   database.collection("books").deleteOne({ id: id }, (err, result) => {
@@ -35,11 +125,30 @@ app.delete("/api/books/:id", (req, res) => {
   });
 });
 
+/**
+ * @swagger
+ * /api/books/{id}:
+ *  get:
+ *      summary: this API is used to get a book given its id
+ *      description: this API is used to get a book given its id
+ *      parameters:
+ *          - in : path
+ *            name: id
+ *            required: true
+ *            description: id number of book to search
+ *            schema:
+ *              type: integer
+ *      responses:
+ *          200:
+ *              description: returns a book
+ *              content:
+ */
+
 app.get("/api/books/:id", (req, res) => {
   database
     .collection("books")
     .find({ id: parseInt(req.params.id) })
-    .toArray((error = r, result) => {
+    .toArray((error, result) => {
       if (error) {
         throw error;
       } else {
@@ -47,6 +156,35 @@ app.get("/api/books/:id", (req, res) => {
       }
     });
 });
+
+/**
+ * @swagger
+ * /api/books/{id}:
+ *  put:
+ *      summary: this API is used to update a book's title
+ *      description: this API is used to update a book's title
+ *      requestBody:
+ *            required: true
+ *            description: the title of the book to update
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  $ref: '#components/schemas/Book'
+ *      parameters:
+ *          - in : path
+ *            name: id
+ *            required: true
+ *            description: id number of book to update
+ *            schema:
+ *              type: integer
+ 
+ *      responses:
+ *          200:
+ *              description: returns a book
+ *              content:
+ *          500:
+ *              description: error
+ */
 
 app.put("/api/books/:id", (req, res) => {
   const title = req.body.title;
@@ -69,6 +207,27 @@ app.put("/api/books/:id", (req, res) => {
       }
     );
 });
+
+/**
+ * @swagger
+ * /api/books/addBook:
+ *  post:
+ *      summary: this API is used to get a book given its id
+ *      description: this API is used to get a book given its id
+ *      requestBody:
+ *            required: true
+ *            description: the title of the book to add
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  $ref: '#components/schemas/Book'
+ *      responses:
+ *          200:
+ *              description: returns a book
+ *              content:
+ *          500:
+ *              description: error
+ */
 
 app.post("/api/books/addBook", (req, res) => {
   //const result = database.collection("books").find().sort({ id: -1 }).limit(1);
